76a124b44a4e981acf84cc08a7935a3f0708f572
Lessyz <102208615+Lessyzz@users.noreply.github.com>
2023-03-16 01:17:32 +0300

    Update LogScanner.py

--- LogScanner.py
+++ LogScanner.py
@@ -1,136 +1,70 @@
 import os
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from tkinter import *
-from tkinter import messagebox
-lang = os.getenv('LANG')
 
-
-def scan():
-    global firstentry
-    global endentry
-    global filepath
-    global savefilepath
-    file = open(filepath).read()
-    firstentry = firstentry.get()
-    endentry = endentry.get()
-    ###### 
-    firstindex = file.index(firstentry)
-    endindex = file.index(endentry)
-    savefileopen = open(savefilepath, "w")
-    for i in range(firstindex,endindex):
-        savefileopen.write(file[i])
-    if "tr_TR" in lang:
-        messagebox.showinfo(title="Tamamlandı!", message="Kaydedildi!")
-    else:
-        messagebox.showinfo(title="Completed!", message="Saved!")
-
-def save():
-    global savefilepath
-    if "tr_TR" in lang:
-        savefilepath = filedialog.askopenfile(mode="w").name
-        savebutton = Button(text="Kaydet", command=scan, bg="black", fg="red", width=8)
-        savebutton.pack()
-        savebutton.place(x=200,y=150)
-    else:
-        savefilepath = filedialog.askopenfile(mode="w").name
-        savebutton = Button(text="Save", command=scan, bg="black", fg="red", width=8)
-        savebutton.pack()
-        savebutton.place(x=200,y=150)
-
-def texts():
-    global filepath
-    global firstentry
-    global endentry
-    if "tr_TR" in lang:
-        filepath = filedialog.askopenfile().name
-        labelfirst = Label(window, bg="black", fg="red", text="Başlangıç metni:")
-        labelfirst.pack()
-        labelfirst.place(x=27,y=68)
-        ######
-        labelend = Label(window, bg="black", fg="red", text="Bitiş metni:")
-        labelend.pack()
-        labelend.place(x=27,y=98)
-        ######
-        first = Entry(window, bg="red", fg="black", width=25)
-        first.pack()
-        first.place(x=120,y=70)
-        ######
-        end = Entry(window, bg="red", fg="black", width=25)
-        end.pack()
-        end.place(x=120,y=100)
-        ######
-        button = Button(text="Kaydedilecek dosyayı seçiniz", command=save, bg="black", fg="red",width=22)
-        button.pack()
-        button.place(x=30,y=150)
-        ######
-        firstentry = first
-        endentry = end
-
-    else:
-        filepath = filedialog.askopenfile().name
-        labelfirst = Label(window, bg="black", fg="red", text="Type first text:")
-        labelfirst.pack()
-        labelfirst.place(x=37,y=68)
-        ######
-        labelend = Label(window, bg="black", fg="red", text="Type last text:")
-        labelend.pack()
-        labelend.place(x=37,y=98)
-        ######
-        first = Entry(window, bg="red", fg="black", width=25)
-        first.pack()
-        first.place(x=120,y=70)
-        ######
-        end = Entry(window, bg="red", fg="black", width=25)
-        end.pack()
-        end.place(x=120,y=100)
-        ######
-        button = Button(text="Select the file to save", command=save, bg="black", fg="red",width=19)
-        button.pack()
-        button.place(x=50,y=150)
-        ######
-        firstentry = first
-        endentry = end
-  
+class LogScanner:
+    def __init__(self):
+        self.gui()
     
-filepath = ""
-savefilepath = ""
-firstentry = ""
-endentry = ""
+    def gui(self):
+        self.window = Tk()
+        self.window.title("Log Scanner")
+        self.window.geometry("300x200")
+        self.window.configure(background="#e0e2db")
+        self.window.resizable(False,False)
 
-if "tr_TR" in lang:
-    window = Tk()
-    window.title("LogScanner by Lessy")
-    window.geometry("300x200")
-    window.configure(background="black")
-    window.resizable(False,False)
-    ######
-    Label2 = Label(window, bg="black", fg="red", text="by Lessy", font="Times 8")
-    Label2.pack()
-    Label2.place(x=250, y=5)
-    ######
-    button = Button(text="Dosyayı seç", command=texts, bg="black", fg="red")
-    button.pack()
-    button.place(x=120,y=20)
-    window.mainloop()
+        # Choose file button
+        self.chooseFileButton = Button(text="Choose file", command=self.entry, bg="#e0e2db", fg="black")
+        self.chooseFileButton.pack()
+        self.chooseFileButton.place(x=120,y=20)
+        self.window.mainloop()
 
+    def entry(self):
+        self.filePath = filedialog.askopenfile().name
 
-else:
-    window = Tk()
-    window.title("LogScanner by Lessy")
-    window.geometry("300x200")
-    window.configure(background="black")
-    window.resizable(False,False)
-    ######
-    Label2 = Label(window, bg="black", fg="red", text="by Lessy", font="Times 8")
-    Label2.pack()
-    Label2.place(x=250, y=5)
-    ######
-    button = Button(text="Choose file", command=texts, bg="black", fg="red")
-    button.pack()
-    button.place(x=120,y=20)
-    window.mainloop()
+        # First letter label
+        self.firstLetterLabel = Label(self.window, bg="#e0e2db", fg="black", text="First letter:")
+        self.firstLetterLabel.pack()
+        self.firstLetterLabel.place(x=27,y=68)
+        # Last letter label
+        self.lastLetterLabel = Label(self.window, bg="#e0e2db", fg="black", text="Last letter:")
+        self.lastLetterLabel.pack()
+        self.lastLetterLabel.place(x=27,y=98)
+        # First letter entry
+        self.firstLetterEntry = Entry(self.window, bg="#e0e2db", fg="black", width=25)
+        self.firstLetterEntry.pack()
+        self.firstLetterEntry.place(x=120,y=70)
+        # Last letter entry
+        self.lastLetterEntry = Entry(self.window, bg="#e0e2db", fg="black", width=25)
+        self.lastLetterEntry.pack()
+        self.lastLetterEntry.place(x=120,y=100)
+        # Button
+        self.SelectFileForSaveButton = Button(text="Select the file to save", command=self.save, bg="#e0e2db", fg="black",width=22)
+        self.SelectFileForSaveButton.pack()
+        self.SelectFileForSaveButton.place(x=30,y=150)
 
+    def save(self):
+        self.saveFilePath = filedialog.askopenfile(mode="w").name # Get save file path
+        # Save Button
+        self.saveButton = Button(text="Save", command=self.saveButtonFunction, bg="#e0e2db", fg="black", width=8)
+        self.saveButton.pack()
+        self.saveButton.place(x=200,y=150)
 
-
-
-
+    def saveButtonFunction(self):
+        self.file = open(self.filePath).read()
+        # Entrys
+        self.firstLetter = self.firstLetterEntry.get()
+        self.lastLetter = self.lastLetterEntry.get()
+        # Getting index of letters
+        self.firstIndex = self.file.index(self.firstLetter)
+        self.lastIndex = self.file.index(self.lastLetter)
+        # File process
+        self.openSaveFile = open(self.saveFilePath, "w")
+        for i in range(self.firstIndex,self.lastIndex):
+            self.openSaveFile.write(self.file[i])
+        
+        messagebox.showinfo(title="Completed!", message="Saved!")
+        self.openSaveFile.close()
+        os.system(str(self.saveFilePath))
+        
+Run = LogScanner()
