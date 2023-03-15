a8bced04180a6ce4913f64002c111655c13a3b09
Lessyz <102208615+Lessyzz@users.noreply.github.com>
2023-03-16 01:25:50 +0300

    Update LogScannerConsole.py

--- LogScannerConsole.py
+++ LogScannerConsole.py
@@ -1,43 +1,26 @@
-import os
-lang = os.getenv('LANG')
 from tkinter import *
+import os
 
-if "tr_TR" in lang:
-    print("""
-        Tüm dosya yolunu yazınız: 
-        Örneğin:/ 'C:/aa.txt'
-        """)
-    file_input = input()
-    file = open(file_input)
-    giris = file.read()
-    first = input("Başlangıç metni: ")
-    end = input("Bitiş metni: ")
-    firstindex = giris.index(first)
-    endindex = giris.index(end)
+os.system("cls")
+print("""
+    Type full PATH: 
+    Example: 'C:/file.txt'
+    """)
 
-    secondfile = input("Kaydedilecek dosyanın yolunu yazınız: ")
-    secondfileopen = open(secondfile, "w")
+filePath = input() # Get file path
+# File process
+file_open = open(filePath)
+file_read = file_open.read()
+# Get inputs
+first = input("Type first letter: ")
+last = input("Type last letter: ")
+# Get indexs
+firstindex = file_read.index(first)
+lastindex = file_read.index(last)
+# Save file
+saveFile = input("Type the path to the file to save: ")
+saveFileOpen = open(saveFile, "w")
 
-    for i in range(firstindex,endindex):
-        secondfileopen.write(giris[i])
-    print("Kaydedildi!")
-
-else:
-    print("""
-        Type full PATH: 
-        Example:/ 'C:/aa.txt'
-        """)
-    file_input = input()
-    file = open(file_input)
-    giris = file.read()
-    first = input("Type first text: ")
-    end = input("Type last text: ")
-    firstindex = giris.index(first)
-    endindex = giris.index(end)
-
-    secondfile = input("Type the path to the file to save: ")
-    secondfileopen = open(secondfile, "w")
-
-    for i in range(firstindex,endindex):
-        secondfileopen.write(giris[i])
-    print("Saved!")
+for i in range(firstindex,lastindex):
+    saveFileOpen.write(file_read[i])
+print("Saved!")
